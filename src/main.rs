44dd6a191044a40836e15695bd84fa0ada614fac
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2022-03-08 14:13:47 -0300

    First commit

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+#![feature(try_blocks)]
+
+use std::{env, fs, io::BufReader, path::Path};
+
+use anyhow::{Context, Result};
+use serde_json::{json, Value};
+
+struct Court {
+    name: String,
+    acronym: String,
+}
+
+struct Courts {
+    inner: Vec<Court>,
+}
+
+impl Courts {
+    pub fn from_file(path: impl AsRef<Path>) -> Result<Self> {
+        let file = fs::File::open(path)?;
+        let buf_reader = BufReader::new(file);
+        let json: Value = serde_json::from_reader(buf_reader)?;
+        let courts: Option<Vec<Court>> = try {
+            dbg!(json.get("items")?.is_array());
+            let mut courts = Vec::with_capacity(212);
+            let items = json.get("items")?.as_array()?;
+            let one = &json!(1);
+
+            for item in items {
+                let map = match item.as_object() {
+                    Some(map) => map,
+                    None => {
+                        println!("{item} is not a map");
+                        continue;
+                    }
+                };
+
+                if map.get("busca_documento")? == one {
+                    let court = Court {
+                        name: map.get("nome")?.as_str()?.to_string(),
+                        acronym: map.get("sigla")?.as_str()?.to_string(),
+                    };
+
+                    courts.push(court);
+                }
+            }
+
+            courts
+        };
+
+        let courts = courts.with_context(|| "failed to parse courts")?;
+        dbg!(courts.len());
+
+        Ok(Self { inner: courts })
+    }
+
+    pub fn display_postgres_enum(&self) {
+        println!("CREATE TYPE kyc.courts AS enum(");
+        for court in self.inner.iter() {
+            println!("\t\"{}\"", court.acronym);
+        }
+        println!(");")
+    }
+}
+
+fn main() -> Result<()> {
+    let path = env::args_os()
+        .nth(1)
+        .with_context(|| "Filename not supplied")?;
+    Courts::from_file(path)?.display_postgres_enum();
+
+    Ok(())
+}
